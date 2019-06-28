416cf3f5c4ef9af852abee884cc47ff433cfa8ba
AshokNaik009 <naikashok08@gmail.com>
2019-06-28 22:21:02 +0530

    Added Spinkit Package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,8 @@
-
-
 import 'package:flutter/material.dart';
 import '../services/location.dart';
-
 import '../services/networking.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import './location_screen.dart';
 
 const apiKey = '003a9db9cdfdad7bf90598847be034f8';
 
@@ -14,7 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   double latitude;
   double longitude;
 
@@ -23,8 +20,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocationData();
-
-
   }
 
   void getLocationData() async {
@@ -32,19 +27,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
-    NetworkHelper networkHelper = new NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    var weatherData =  await networkHelper.getData();
-    print(weatherData);
-
+    NetworkHelper networkHelper = new NetworkHelper(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    //print(weatherData);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LocationScreen()),
+    );
   }
-
-  
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(),
+      body: Center(
+        child: SpinKitFadingCircle(
+          itemBuilder: (_, int index) {
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                color: index.isEven ? Colors.red : Colors.green,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
